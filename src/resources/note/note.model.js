1e75534e533af6d5d648f195a2e49b8d69fd70ba
GuptaEesh <p1r8a5t7ibha@gmail.com>
2022-05-14 17:33:56 +0530

    [chore]: added 2 more fields in note model

--- src/resources/note/note.model.js
+++ src/resources/note/note.model.js
@@ -30,6 +30,14 @@ const noteSchema = new mongoose.Schema(
       type: Number,
       required: true,
     },
+    isArchived: {
+      type: Boolean,
+      required: true,
+    },
+    isTemporarilyDeleted: {
+      type: Boolean,
+      required: true,
+    },
     createdBy: {
       type: mongoose.SchemaTypes.ObjectId,
       ref: "user",
